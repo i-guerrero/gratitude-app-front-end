ba976624a17b03d97c2b7328215817f1523c19af
i-guerrero <isaacguerrero@pursuit.org>
2023-02-12 13:19:16 -0500

    Add Bootstrap and add NavBar

---
+++ src/Pages/Home.js
@@ -0,0 +1,17 @@
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <Container>
+        <h1>Welcome to the Gratitude App</h1>
+        <p>This is a gratitude journal.</p>
+        <Link to="/entries">
+          <Button variant="dark">View List of Journal Entries</Button>
+        </Link>
+      </Container>
+    </div>
+  );
+}
